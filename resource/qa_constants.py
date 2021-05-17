cea14d2964dc1c25b96a71f94dfa76e6acc45e59
nchandr2 <nishantsinghchandra@gmail.com>
2021-05-17 12:14:13 +0530

    reduced number of lines in code, \n Improvised the logics, \n added directories for further developments.

---
+++ resource/qa_constants.py
@@ -0,0 +1,39 @@
+qaar = 'QAAR'
+qat = 'QAT'
+govqa = 'GovQA'
+
+
+class qa_constants:
+    def __init__(self, env):
+        self.env = env
+        if env == qat:
+            self.link = 'http://jenkins.shn.io/job/tp-automation-batch-py3/'
+            self.img = 'http://jenkins.shn.io/buildStatus/icon?job=tp-automation-batch-py3'
+            self.gap_analysis = 'http://172.18.19.196:5253/tpEmrGapAnalysis.html'
+
+
+
+        elif env == qaar:
+            self.link = 'http://jenkins.shn.io/job/tp-automation-batch/'
+            self.img = 'http://jenkins.shn.io/buildStatus/icon?job=tp-automation-batch/'
+            self.gap_analysis = 'http://172.18.67.93:5253/tpEmrGapAnalysis.html'
+
+        elif env == govqa:
+            self.link = 'http://jenkins.shn.io/job/tp-automation-batch/'
+            self.img = 'http://jenkins.shn.io/buildStatus/icon?job=tp-automation-batch/'
+            self.gap_analysis = 'http://172.18.67.93:5253/tpEmrGapAnalysis.html'
+
+    # @staticmethod
+    def get_constants_json(self):
+        constants = {
+            'link': self.link,
+            'img': self.img,
+            'gap_analysis': self.gap_analysis,
+            'env': self.env
+        }
+        return constants
+
+
+if __name__ == "__main__":
+    qat = qa_constants('QAT')
+    print qat.link
